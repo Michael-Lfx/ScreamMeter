1cf1c5adf7bc7270b4a52fc8df7acbf21f06b337
Amol Chaudhari <chaudhari.amol.sopan@gmail.com>
2014-02-23 14:49:36 -0500

    Added rate us and More games functionality

--- ScreamMeter/MainMenuViewController.m
+++ ScreamMeter/MainMenuViewController.m
@@ -12,6 +12,9 @@
 
 @interface MainMenuViewController ()
 
+-(IBAction)openMoreGames:(id)sender;
+
+-(IBAction)rateUs:(id)sender;
 
 @end
 
@@ -24,6 +27,18 @@
         // Custom initialization
     }
     return self;
+}
+
+
+-(IBAction)openMoreGames:(id)sender{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://itunes.apple.com/us/artist/appagogo/id552771306"]];
+}
+
+
+//https://itunes.apple.com/us/app/screammeter/id827482116?ls=1&mt=8
+-(IBAction)rateUs:(id)sender{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://itunes.apple.com/us/app/screammeter/id827482116?ls=1&mt=8"]];
+
 }
 
 
